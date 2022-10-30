ba14eb72f3db404a385661f34bd3f36e8fb7f742
Alex Cheung <alexkwokoncheung@AC.local>
2022-10-29 19:30:08 -0700

    finished basic layout on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
+import { Coins } from "./types";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const [coins, setCoins] = useState<Coins[]>([]);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      const { data } = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      );
+
+      setCoins(data);
+    };
+
+    fetchCoins();
+  }, []);
+  return (
+    <div className="app">
+      <h1>Crypto Space</h1>
+    </div>
+  );
+};
 
 export default App;
